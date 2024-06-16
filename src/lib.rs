eea4e7b76757f621ae1ab073aaea52e097bd9292
ettolrach <ettolrach@disroot.org>
2024-06-17 00:31:20 +0100

    Add licence information

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+//     Copyright 2024 Charlotte Ausel
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use std::{fmt::Display, iter::Sum};
 
 const CURRENCIES_AS_HALFPENCE: [usize; 11] = [1, 2, 6, 12, 24, 48, 60, 120, 480, 2400, 4800];
